e31eec3d873d4ddc0e7b3530df8c271b21c2d0ab
juniorSarh <sabelohgumede@gmail.com>
2025-10-14 12:25:17 +0200

    added nav and footer on project page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,14 @@
-
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      <Navbar/>
+              <div style={{height:'650px'}}>
+      
+              </div>
+              <Footer/>
+      </div>
   )
 }
