6cd9e926e3f22d6c2eb92bece6496c56f6ff9243
juniorSarh <sabelohgumede@gmail.com>
2025-12-12 10:06:39 +0200

    npm run build for testing and deployment

--- src/pages/TechStack.tsx
+++ src/pages/TechStack.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
